8ed014b465303a9c9b31bcb3bd71eb44bcd7cba4
Romich123 <sepitour1998@gmail.com>
2023-09-28 20:10:31 +0800

    signal setter argument can be function

--- src/signals.ts
+++ src/signals.ts
@@ -8,22 +8,24 @@ type Context = {
 let currentContext: Context | null = null
 
 export type Getter<T> = () => T
-export type Setter<T> = (t: T) => void
+export type Setter<T> = (t: T | (() => T)) => void
 
 export function createSignal<T>(defaultVal: T) {
     let val = defaultVal
     const contexts: Map<Context, true> = new Map()
 
-    const getter = () => {
-        if (currentContext) contexts.set(currentContext, true)
-
-        return val
-    }
-
     return [
-        getter,
+        () => {
+            if (currentContext) contexts.set(currentContext, true)
+
+            return val
+        },
         (v: T) => {
-            val = v
+            if (typeof v === "function") {
+                val = v()
+            } else {
+                val = v
+            }
 
             for (let context of contexts.keys()) {
                 context.fn?.()
